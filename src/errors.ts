e2a45f7438c65fe160b6e1f482bbf729e5d15f02
maishu <ansiboy@163.com>
2019-12-31 14:55:47 +0800

    添加 Errors 类

--- src/errors.ts
+++ src/errors.ts
@@ -1,3 +1,23 @@
+export class Errors {
+    argumentNull(argumentName: string) {
+        let error = new Error(`Argument ${argumentName} cannt be null or emtpy.`);
+        error.name = Errors.prototype.argumentNull.name;
+        return error;
+    }
+    routeDataFieldNull(fieldName: string): Error {
+        let msg = `The ${fieldName} field of route data cannt be null.`;
+        let error = new Error(msg);
+        error.name = Errors.prototype.routeDataFieldNull.name;
+        return error;
+    }
+    argumentFieldNull(fieldName: string, argumentName: string): Error {
+        let msg = `The ${fieldName} field of ${argumentName} cannt be null.`;
+        let error = new Error(msg);
+        error.name = Errors.prototype.argumentFieldNull.name;
+        return error;
+    }
+
+}
 export let errors = {
     serviceUrlCanntNull(serviceName: string) {
         let msg = `Service '${serviceName}' base url can not null.`
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export { Service, AjaxOptions, IService, ServiceConstructor } from './service'
-export { Callback, Callback0, Callback1, Callback2, Callbacks } from './callback'
-export { ValueStore, LocalValueStore, CookieValueStore } from './value-store'
-export { guid } from "./utility"
+export { Service, AjaxOptions, IService, ServiceConstructor } from './service';
+export { Callback, Callback0, Callback1, Callback2, Callbacks } from './callback';
+export { ValueStore, LocalValueStore, CookieValueStore } from './value-store';
+export { guid } from "./utility";
+export { Errors } from "./errors";
