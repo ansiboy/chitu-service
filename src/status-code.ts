f7cfedb319fddcfe1b0d0fda82f87edc8d246326
maishu <ansiboy@163.com>
2020-01-08 22:28:25 +0800

    添加状态码

--- src/status-code.ts
+++ src/status-code.ts
@@ -1,28 +1,35 @@
-export enum StatusCodes {
+export enum StatusCode {
+
     //=============================================
     // 成功的状态码，必须大于 200
-    login = 282,
-    logout = 283,
+    Login = 282,
+    Logout = 283,
     //=============================================
-    forbidden = 403,
+
+    NotFound = 404,
+    OK = 200,
+    Redirect = 301,
+    BadRequest = 400,
+    Forbidden = 403,
+
     //=============================================
     // 失败的状态码，必须小于或等于 700
-    argumentNull = 705,
+    ArgumentNull = 705,
 
     /** 字段为空 */
-    fieldNull = 709,
+    FieldNull = 709,
 
-    argumentTypeIncorrect = 710,
+    ArgumentTypeIncorrect = 710,
 
     /** 用户未登录 */
-    userNotLogin = 718,
+    UserNotLogin = 718,
 
     /** 没有权限 */
-    noPermission = 726,
-    userIdNull = 727,
+    NoPermission = 726,
+    UserIdNull = 727,
 
     /** 指定 ID 的对象不存在 */
-    objectNotExistWithId = 728,
+    ObjectNotExistWithId = 728,
 }
 
 
